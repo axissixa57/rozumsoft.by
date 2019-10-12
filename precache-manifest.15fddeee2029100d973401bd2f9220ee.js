ecf30e8e3afe3ab3c504fd24195a0b4bd1709b66
Ax1S <axissixa@mail.ru>
2019-10-12 12:44:41 +0300

    Updates

---
+++ precache-manifest.15fddeee2029100d973401bd2f9220ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6e891ec4b0aa67b8b8bedf7fcd19cf",
+    "url": "/rozumsoft.by/index.html"
+  },
+  {
+    "revision": "1c1621ffc801b38a4cec",
+    "url": "/rozumsoft.by/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "d4912d221f6aeff10e8c",
+    "url": "/rozumsoft.by/static/js/2.ecc9f0ee.chunk.js"
+  },
+  {
+    "revision": "1c1621ffc801b38a4cec",
+    "url": "/rozumsoft.by/static/js/main.fbcd96e3.chunk.js"
+  },
+  {
+    "revision": "d79432a07a598d8751db",
+    "url": "/rozumsoft.by/static/js/runtime-main.5600e354.js"
+  }
+]);
